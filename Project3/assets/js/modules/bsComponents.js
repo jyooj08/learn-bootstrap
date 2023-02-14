6c4709d1e63aeaaaf936f912692c5cd8eb86de4b
jyooj08 <jyooj08@naver.com>
2023-02-14 18:49:51 +0900

    Project3 post content 1

--- Project3/assets/js/modules/bsComponents.js
+++ Project3/assets/js/modules/bsComponents.js
@@ -1,9 +1,13 @@
 export function bs() {
   // Bootstrap5 tooltip
-  var tooltipTriggerList = [].slice.call(
-    document.querySelectorAll('[data-bs-toggle="tooltip"]')
-  );
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+
+  // Bootstrap popover
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
   });
 }
